10a30d987c97f9a67b9e95360efd78176c21318c
Sakke <sakke.suomalainen@gmail.com>
2025-04-21 22:43:42 +0300

    fix tiliryhmä label not showing

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,11 +1,12 @@
 import dropdowns from '../data/dropdowns.json'
 
 function getLabel(code, level) {
-  const dropdown = dropdowns.find(d => d.dropdown === level)
-  if (!dropdown) return ''
-  const found = dropdown.options.find(opt => opt.code === code)
-  return found ? `${code} – ${found.label}` : code
-}
+    const dropdown = dropdowns.find(d => d.dropdown === level)
+    if (!dropdown) return code
+    const found = dropdown.options.find(opt => opt.code === code)
+    return found ? `${code} – ${found.label}` : code
+  }
+  
 
 export function formatCOFOG({ cofog1, cofog2, cofog3 }) {
   return [cofog1 && getLabel(cofog1, 1), cofog2 && getLabel(cofog2, 2), cofog3 && getLabel(cofog3, 3)].filter(Boolean)
