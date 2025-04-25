456e5d5a4754b99e062c24c9444fb7a019929d7d
Sakke <sakke.suomalainen@gmail.com>
2025-04-25 16:48:56 +0300

    fix 7

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
   useNavigate,
 } from 'react-router-dom'
 
-import { supabase } from '../../lib/supabaseClient'   // path from src/App.jsx
+import { supabase } from '../lib/supabaseClient'   // path from src/App.jsx
 import Container   from './components/layout/Container'
 
 import Home     from './routes/Home'
