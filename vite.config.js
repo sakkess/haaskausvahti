d0655ab3366487d678e135e44aea17a767685149
Sakke <sakke.suomalainen@gmail.com>
2025-04-26 20:10:49 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [
+    react(),      // enables JSX support
+    tailwindcss() // processes your tailwind.config.js
+  ],
+  resolve: {
+    alias: {
+      '@lib': '/src/lib'
+    }
+  }
+})
